7813d899e2d97cd4fb9b7e8f3b6a8b4d1f467ea2
Gaurav Abbi <abbi.gaurav@gmail.com>
2018-12-10 17:45:29 +0100

    Set up k8s-io/client
    - Add to the toml file
    - Create skelton structure for Controller

--- my-awesome-controller/controller/CakeController.go
+++ my-awesome-controller/controller/CakeController.go
@@ -1,1 +1,13 @@
 package controller
+
+import (
+	"github.com/abbi-gaurav/go-learning-projects/my-awesome-controller/db"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+)
+
+type CakeController struct {
+	db        db.DB
+	informer  cache.SharedIndexInformer
+	workQueue workqueue.RateLimitingInterface
+}
