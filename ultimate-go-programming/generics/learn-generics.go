479ff9bbd46c79da62cf4c12b6426e19393442da
Gaurav Abbi <gaurav.abbi@sap.com>
2022-05-25 17:24:58 +0200

    Update to 1.18
    - Update the mod file
    - Add example for using generics

---
+++ ultimate-go-programming/generics/learn-generics.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+type Subtractable interface {
+	~int | int32 | float64 | float32
+}
+
+type Results[T Subtractable] []T
+
+type MyOwnInteger int
+
+type Movable[S Subtractable] interface {
+	move(S)
+}
+
+type Person[S Subtractable] struct {
+	name string
+}
+
+func (p Person[S]) move(meters S) {
+	fmt.Printf("%s moved %d meters\n", p.name, meters)
+}
+
+func main() {
+	fmt.Println(subtract(1, 0))
+	fmt.Println(subtract(12.4, 13.8))
+	fmt.Println(subtract2(14.4, 13.8))
+	fmt.Println(subtract2[int](2, 3))
+
+	var myInt MyOwnInteger
+	myInt = 10
+	fmt.Println(subtract2(myInt, 9))
+
+	var resultStorage Results[int]
+	resultStorage = append(resultStorage, subtract[int](5, 8))
+	fmt.Println("ResultStorage:", resultStorage)
+
+	p := Person[int]{name: "John"}
+	fmt.Println(move(p, 10, 2))
+}
+
+func move[V Movable[S], S Subtractable](v V, distance S, meters S) S {
+	v.move(meters)
+	return subtract2(distance, meters)
+}
+
+func subtract[V int64 | int | float32 | float64](a, b V) V {
+	return a - b
+}
+
+func subtract2[V Subtractable](a, b V) V {
+	return a - b
+}
--- ultimate-go-programming/go.mod
+++ ultimate-go-programming/go.mod
@@ -1,10 +1,11 @@
 module github.com/abbi-gaurav/go-projects/ultimate-go-programming
 
-go 1.13
+go 1.18
+
+require github.com/stretchr/testify v1.2.0
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.1.0 // indirect
-	github.com/stretchr/testify v1.2.0
 )
